4f06b6e44bbba9333faebdbbd5e588a690000301
AlexinaO <alexina.orsoni@gmail.com>
2018-08-08 12:33:41 +0200

    Création classe Auteur

---
+++ Bibliotheque/Classes/Auteur.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibliotheque.Classes
+{
+    public sealed class Auteur
+    {
+        public int ID { get; private set; }
+        public string Nom { get; private set; }
+        public string Prenom { get; private set; }
+
+
+    }
+}
