b7ffde07c7c3738c2994234fb3d72ddaeee1389f
AlexinaO <alexina.orsoni@gmail.com>
2018-08-08 14:00:54 +0200

    Classe ouvrage avec relation auteur

--- Bibliotheque/Classes/Ouvrage.cs
+++ Bibliotheque/Classes/Ouvrage.cs
@@ -8,11 +8,28 @@ namespace Bibliotheque.Classes
 {
     public abstract class Ouvrage
     {
+        public Ouvrage()
+        {
+            Titre = "tritre manquant";
+            Auteurs = new List<Auteur>();
+        }
+
+        public Ouvrage(string filiere, string categorie, string discipline, string titre)
+        {
+            Filiere = filiere;
+            Categorie = categorie;
+            Discipline = discipline;
+            Titre = titre;
+            Auteurs = new List<Auteur>();
+        }
+
         public int IdOuvrage { get; private set; }
         public string Filiere { get; private set; }
         public string Categorie { get; private set; }
         public string Discipline { get; private set; }
         public string Titre { get; private set; }
 
+        public List<Auteur> Auteurs { get; private set; }
+        
     }
 }
