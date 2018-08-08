84ed2306871cdf3d16b559c5dd04ada0609faf2e
AlexinaO <alexina.orsoni@gmail.com>
2018-08-08 11:56:13 +0200

    Début des classe User, Prof et Etudiant

---
+++ Bibliotheque/Classes/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibliotheque.Classes
+{
+    public abstract class User
+    {
+        public int IdCarte { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Email { get; set; }
+
+    }
+}
