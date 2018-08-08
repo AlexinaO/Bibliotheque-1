7f292f9a9a3190e267e0a34749074a6c813732a8
AlexinaO <alexina.orsoni@gmail.com>
2018-08-08 15:42:25 +0200

    Avec la classe Emprunte sans méthode effacer

---
+++ Bibliotheque/Classes/Emprunte.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibliotheque.Classes
+{
+    public enum NbOuvragesEmpruntes
+    {
+        UN = 1,
+        DEUX = 2
+    };
+
+    public class Emprunte
+    {
+        public DateTime DateEmprunt { get; private set; }
+        public User User { get; private set; }
+        public Ouvrage[] OuvrageEmprunte { get; private set; }
+
+        public Emprunte(DateTime dateEmprunt, User user, NbOuvragesEmpruntes nb)
+        {
+            DateEmprunt = dateEmprunt;
+            User = user;
+            OuvrageEmprunte = new Ouvrage[(int)nb];
+        }
+
+        // methode Effacer() efface la réservation quand l'user emprunte
+        public void Effacer()
+        {
+
+        }
+    }
+}
